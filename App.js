d1c941f959ca333baaec96188352ccfca388b0f4
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-04-10 17:20:15 +0530

    explored components and JSX in react

--- App.js
+++ App.js
@@ -1,39 +1,28 @@
-// const heading = React.createElement("h1", {
-//     id: "head"
-// }, "Hello world fom ReactJS"); //here heading is not a HTML h1 tage , instead it is a JS object or just a react element.
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading); //internally creates the tree like structure of the DOM by taking the object , converting it into a HTML tag and put it on the DOM.
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-/* <div id = "parent">
-    <div id = "child">
-        <h1></h1>
-    </div>
-</div> */
+const title = < h1 > Hello from title < /h1>;
 
+const Footer = () => {
+    return ( <
+        div >
+        <
+        h2 > this is the footer < /h2> < /
+        div >
+    )
+}
 
-// const parent = React.createElement("div", {
-//     id: "parent"
-// }, React.createElement("div", {
-//     id: "child"
-// }, React.createElement("h1", {}, "h1 created by react")));
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-
-/* <div id = "parent">
-    <div id = "child">
-        <h1></h1>
-        <h2></h2>
-    </div>
-</div> */
-
-//if a tage has multiple children , then wwe should pass in the array and then use React.createElement("" , {},"...")
-const parent = React.createElement("div", {
-    id: "parent"
-}, React.createElement("div", {
-    id: "child"
-}, [React.createElement("h1", {}, "h1 created by react"), React.createElement("h1", {}, "h2 created by react")]));
+const Header = () => {
+    return ( <
+        div > {
+            title
+        } <
+        Footer / >
+        <
+        h2 > h2 heading inside the Header component hey < /h2> < /
+        div >
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render( < Header / > );
