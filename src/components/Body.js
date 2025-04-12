1818d0b972467f26d6fbf571e7a2915d47eef6c1
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-04-12 15:03:01 +0530

    added search feature in the app

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,55 @@
 import RestaurantCard from './RestaurantCard';
 import indianRestaurants from '../utils/mockData';
-import { useState } from 'react';
+import Shimmer from './shimmer';
+import { useState, useEffect } from 'react';
 
 const Body = () => {
   const [State, setState] = useState(indianRestaurants);
+  const [searchText, setSearchText] = useState('');
+  const [filteredRest, setFilteredRest] = useState(indianRestaurants);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+    );
+    //as soon as we have got this data , now we have rerender the compoenents . spo we have to update the State using setState() function
+    const json = await data.json();
+    const infoArray =
+      json?.data?.cards?.[0]?.card?.gridElements?.infoWithStyle?.info || [];
+    //setState(infoArray);
+  };
+
+  //Conditional rendering
+  if (State.length === 0) {
+    return <Shimmer />;
+  }
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRest = State.filter((res) =>
+                res.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setFilteredRest(filteredRest);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -18,7 +61,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {State.map((restaurant) => {
+        {filteredRest.map((restaurant) => {
           return <RestaurantCard key={restaurant.id} resData={restaurant} />;
         })}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO } from '../utils/constants';
+import { useState } from 'react';
 
 const Header = () => {
+  const [State, setState] = useState('login'); //here 'login' is the default initial value
   return (
     <div className="header">
       <div className="logo-container">
@@ -10,6 +12,14 @@ const Header = () => {
         <ul>
           <li> Home </li> <li> About Us </li> <li> Contact us </li>{' '}
           <li> Cart </li>{' '}
+          <button
+            className="login"
+            onClick={() => {
+              State == 'login' ? setState('logout') : setState('login');
+            }}
+          >
+            {State}
+          </button>
         </ul>{' '}
       </div>{' '}
     </div>
