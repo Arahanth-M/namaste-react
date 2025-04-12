b97bf2dd926462504b976c2573f851da820f98f0
Arahanth-M <151705727+Arahanth-M@users.noreply.github.com>
2025-04-12 09:59:15 +0530

    useState and useeffect hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,24 @@
 import RestaurantCard from './RestaurantCard';
 import indianRestaurants from '../utils/mockData';
+import { useState } from 'react';
 
 const Body = () => {
+  const [State, setState] = useState(indianRestaurants);
   return (
     <div className="body">
-      <div className="search">Search </div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const FilteredList = State.filter((res) => res.rating > 4.2);
+            setState(FilteredList);
+          }}
+        >
+          Top rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {indianRestaurants.map((restaurant) => {
+        {State.map((restaurant) => {
           return <RestaurantCard key={restaurant.id} resData={restaurant} />;
         })}
       </div>
